5950ae27b9a2350d0ef26b04b898c2d5a081b51e
Thagus <agu.alcarin@gmail.com>
2017-05-05 01:24:03 -0500

    Main

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import harvester.Facade;
+
+/**
+ * Created by Thagus on 05/05/17.
+ */
+public class Main {
+    public static void main(String[] args){
+        Facade facade = new Facade();
+
+        facade.doHarvest();
+    }
+}
