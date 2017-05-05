f6dc758be7b534ba8b7bdd6cc565c9c6d4e10059
Thagus <agu.alcarin@gmail.com>
2017-05-05 00:51:45 -0500

    Harvesting done from facade

---
+++ src/main/java/harvester/Facade.java
@@ -0,0 +1,34 @@
+package harvester;
+
+import harvester.model.DCHarvester;
+import harvester.model.ModelDatabase;
+import interfaces.IDocument;
+
+/**
+ * Created by Thagus on 05/05/17.
+ */
+public class Facade {
+    private ModelDatabase db;
+    private String SITE = "http://catarina.udlap.mx/tales/oai/";
+
+    public Facade(){
+        this.db = ModelDatabase.instance();
+    }
+
+    public void addDocument(IDocument document){
+        db.docOps.addDocument(document);
+    }
+
+    public void doHarvest(){
+        DCHarvester dcHarvester = new DCHarvester();
+
+        try {
+            dcHarvester.harvest(SITE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //A message to alert the user about the number of read documents
+        System.out.println("Read " + db.docOps.countDocuments() + " documents");
+    }
+}
